37d70e2dca0eb76d15fd6f54b190e909b6f3e366
Christopher Bartling <chris.bartling@gmail.com>
2024-09-09 16:58:15 -0500

    feat: wiring up msw dependency

--- render-perf-tuning/src/main.tsx
+++ render-perf-tuning/src/main.tsx
@@ -1,12 +1,26 @@
-import './wdyr'; // <--- first import
+import './wdyr';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { router } from './router.tsx';
 import { RouterProvider } from 'react-router-dom';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  );
+});
