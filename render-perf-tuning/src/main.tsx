17fb850f07a25473788b3caf41d564dfb095534e
Christopher Bartling <chris.bartling@gmail.com>
2024-09-10 08:31:20 -0500

    feat: wiring up msw

--- render-perf-tuning/src/main.tsx
+++ render-perf-tuning/src/main.tsx
@@ -9,7 +9,6 @@ async function enableMocking() {
   if (process.env.NODE_ENV !== 'development') {
     return;
   }
-
   const { worker } = await import('./mocks/browser');
 
   // `worker.start()` returns a Promise that resolves
